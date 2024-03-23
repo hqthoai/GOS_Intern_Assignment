9ca9b9dc0e3d35a9593347d77c1bdab147af5ebc
Quoc Thoai <thoaifamily83@gmail.com>
2024-03-24 02:03:58 +0700

    add load more func

--- client/src/components/WeeklyForecast/WeeklyForecast.js
+++ client/src/components/WeeklyForecast/WeeklyForecast.js
@@ -30,7 +30,11 @@ const mockData = [
 function WeeklyForecast() {
     return (
         <div className="p-2 bg-slate-100/20 rounded-lg text-white">
-            <div>
+            <div className="flex justify-end items-center px-2 pb-6 hover:cursor-pointer text-[16px]">
+                <div className="border-b-2 ">
+                    Load more
+                    <i className="ml-1 fa-solid fa-angles-right text-sm"></i>
+                </div>
             </div>
             <div className="grid grid-cols-4 gap-2">
                 {
