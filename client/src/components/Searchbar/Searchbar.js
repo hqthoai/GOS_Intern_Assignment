a0f5ea4a8abb876bb00ca24c42d8a2e63823906d
Quoc Thoai <thoaifamily83@gmail.com>
2024-03-25 03:30:47 +0700

    add search result

--- client/src/components/Searchbar/Searchbar.js
+++ client/src/components/Searchbar/Searchbar.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+import getCityFromCoordinates from "../../utils/GetCityName";
 
 function Searchbar({ setCityName }) {
     const [searchInput, setSearchInput] = useState('');
@@ -6,17 +8,72 @@ function Searchbar({ setCityName }) {
     const handleSearch = () => {
         setCityName(searchInput);
     }
-    const handleKeyPress = (event) => {
-        if (event.key === 'Enter') {
-            handleSearch();
+
+    const handleSelect = async (address) => {
+        setSearchInput(address);
+        try {
+            const results = await geocodeByAddress(address);
+            const coordinates = await getLatLng(results[0]);
+            setCityName(await getCityFromCoordinates(coordinates.lat, coordinates.lng))
+        } catch (error) {
+            console.error('Error', error);
         }
-    }
+    };
     return (
-        <div className="flex p-2 bg-slate-100/10 rounded-lg text-sm">
-            <input className="w-full mr-2 px-4 rounded-sm" type="search" value={searchInput} onChange={(e) => setSearchInput(e.target.value)} onKeyPress={handleKeyPress} placeholder="Enter the city name" />
-            <div className="float-left px-3.5 py-2 bg-orange-400 rounded-sm text-white hover:cursor-pointer active:scale-95" onClick={handleSearch} >
+        <div className="flex p-2 bg-slate-100/10 rounded-lg text-sm relative">
+            <PlacesAutocomplete
+                value={searchInput}
+                onChange={setSearchInput}
+                onSelect={handleSelect}
+            >
+                {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                    <div className="w-full rounded-sm h-full mr-2">
+                        <input
+                            {...getInputProps({
+                                placeholder: 'Enter the city name',
+                                className: 'w-full h-full mr-2 px-4 rounded-sm',
+                            })}
+                        />
+
+                        <div className="absolute top-full left-2 bg-white rounded-sm border-gray-300 shadow-md"
+                            style={{ width: 'calc(100% - 16px)' }}>
+                            {loading && <div className="px-4 py-2">Loading...</div>}
+                            {suggestions.map((suggestion, index) => {
+                                const className = 'px-4 py-2 cursor-pointer hover:bg-gray-100';
+                                return (
+                                    <div
+                                        key={index}
+                                        {...getSuggestionItemProps(suggestion, {
+                                            className,
+                                        })}
+                                    >
+                                        {suggestion.description}
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                )}
+            </PlacesAutocomplete>
+            <div className="fl px-3.5 py-2 bg-orange-400 rounded-sm text-white hover:cursor-pointer active:scale-95" onClick={handleSearch} >
                 Search
             </div>
+            {/* <input className="w-full mr-2 px-4 rounded-sm" type="search" value={searchInput} onChange={(e) => setSearchInput(e.target.value)} onKeyPress={handleKeyPress} placeholder="Enter the city name" />
+            <div className="px-3.5 py-2 bg-orange-400 rounded-sm text-white hover:cursor-pointer active:scale-95" onClick={handleSearch} >
+                Search
+            </div>
+            {searchResults.length > 0 && (
+                <div className="absolute top-full left-2 bg-white border rounded-sm border-gray-300 shadow-md max-h-48 overflow-y-auto "
+                    style={{ width: 'calc(100% - 16px)' }}>
+                    <ul >
+                        {searchResults.map((result, index) => (
+                            <li key={index} className="px-4 py-2 cursor-pointer hover:bg-gray-100">
+                                {result}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )} */}
         </div>
     );
 }
